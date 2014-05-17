0a85b99ced8bd1deba5ffcded2c6e56df71d9c3c
Charlie Jones <myupstateproperties@gmail.com>
2014-05-17 13:44:43 -0400

    fix dot button on top taskbar and add avatar and image that pulls data from API

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,3 +21,33 @@ function renderProfile (profile){
         var rendered = getProfileTemplate(profile);
         $('.sidebar').append(rendered);
     }
+
+
+var getHeaderInfo = _.template($('.headerInfo').text());
+$.getJSON('https://api.github.com/users/Charlie610790').done(function(header){
+    renderHeader(header);
+});
+
+function renderHeader (header){
+        var rendered = getHeaderInfo(header);
+        $('.topuserfield').append(rendered);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
