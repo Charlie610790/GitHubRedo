f2063e6f7e337bc502ee52958ae0d235442b9619
Charlie Jones <myupstateproperties@gmail.com>
2014-05-17 15:03:45 -0400

    Trying to push to gh-pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 var getNameTemplate = _.template($('.repo-row').text());
-$.getJSON('https://api.github.com/users/Charlie610790/repos').done(function(repos){
+$.getJSON('https://api.github.com/users/Charlie610790/repos?client_id=ae0cd330a00ca9c3c446&client_secret=a9ba76c532a3c0dca1907511ae377bee64532a47').done(function(repos){
     renderRepos(repos);
 });
 
@@ -13,7 +13,7 @@ function renderRepos (repos){
 }
 
 var getProfileTemplate = _.template($('.profile').text());
-$.getJSON('https://api.github.com/users/Charlie610790').done(function(profile){
+$.getJSON('https://api.github.com/users/Charlie610790?client_id=ae0cd330a00ca9c3c446&client_secret=a9ba76c532a3c0dca1907511ae377bee64532a47').done(function(profile){
     renderProfile(profile);
 });
 
@@ -24,7 +24,7 @@ function renderProfile (profile){
 
 
 var getHeaderInfo = _.template($('.headerInfo').text());
-$.getJSON('https://api.github.com/users/Charlie610790').done(function(header){
+$.getJSON('https://api.github.com/users/Charlie610790?client_id=ae0cd330a00ca9c3c446&client_secret=a9ba76c532a3c0dca1907511ae377bee64532a47').done(function(header){
     renderHeader(header);
 });
 
